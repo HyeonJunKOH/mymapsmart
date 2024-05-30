2af69a3104a0646c14e5a21ede6642200a702828
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-05-30 14:56:26 +0900

    5월30일 main컴포넌트 구현 중

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import "./Main.css"
 
 function Main(){
+    useEffect(()=>{
+
+
+
+
+    })
     return(
         <div className="main_wrapper">
-            <h4>MapsMart 란?</h4>
+            <div className="main_title1">지금 바로! 원하는 장소를 담아보세요!</div>
+            <div className="main_title2">나만의 Maps! Mart~</div>
+            <div className="main_title3">음식점, 명소, 쇼핑, 숙박까지! 대전의 모든 핫플레이스를 만나보세요. 장소를 골라 나만의 장바구니를 만들고 공유할 수 있습니다.</div>
         </div>
     )
 }
