ef972f05cf7ae72deda8670eeddc464de99a4b21
KOHHyeonJun <tttmmm100@naver.com>
2024-06-07 18:38:10 +0900

    6월7일 메뉴바 링크 연동

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import "./Menu.css";
 
 function Menu(){
+    const navigate = useNavigate();
+
+    const handleTour = () =>{
+        navigate(`/tour`)
+    };
+    const handleFood = () =>{
+        navigate(`/food`)
+    };
+    const handleHotel = () =>{
+        navigate(`/hotel`)
+    };
+    const handleShp = () =>{
+        navigate(`/shp`)
+    };
+
     return(
         <>
             <div className="menu_wrapper">
-                <div className="menu_tour">관광지</div>
-                <div className="menu_food">음식점</div>
-                <div className="menu_hotel">숙박시설</div>
-                <div className="menu_shp">쇼핑몰</div>
+                <div className="menu_tour" onClick={handleTour}>관광지</div>
+                <div className="menu_food" onClick={handleFood}>음식점</div>
+                <div className="menu_hotel" onClick={handleHotel}>숙박시설</div>
+                <div className="menu_shp" onClick={handleShp}>쇼핑몰</div>
             </div>
         </>
     )
