9cbb114d34fddde2347be95bb2c883772035306c
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-06-20 17:12:52 +0900

    6월 20일 vercel 설치 및 api 경로 수정

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-const BASE_URL1 = "http://apis.data.go.kr/6300000/openapi2022/tourspot/gettourspot"
-const BASE_URL2 = "http://apis.data.go.kr/6300000/openapi2022/shppg/getshppg"
-const BASE_URL3 = "http://apis.data.go.kr/6300000/openapi2022/restrnt/getrestrnt"
-const BASE_URL4 = "http://apis.data.go.kr/6300000/openapi2022/tourroms/gettourroms"
+const BASE_URL1 = "https://apis.data.go.kr/6300000/openapi2022/tourspot/gettourspot"
+const BASE_URL2 = "https://apis.data.go.kr/6300000/openapi2022/shppg/getshppg"
+const BASE_URL3 = "https://apis.data.go.kr/6300000/openapi2022/restrnt/getrestrnt"
+const BASE_URL4 = "https://apis.data.go.kr/6300000/openapi2022/tourroms/gettourroms"
 
 export const TourUrl = `${BASE_URL1}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
 export const ShpUrl = `${BASE_URL2}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
