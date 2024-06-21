25952504f231a058e328eb9ac9f37cd51fd12724
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-06-21 18:19:49 +0900

    카카오 공유 기능 보류

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import KakaoShareButton from "../components/KakaoShare";
 import Main from "../components/Main";
 
 function Home(){
@@ -8,9 +7,6 @@ function Home(){
         <>
             <Header title={"MapsMart"}/>
             <Main/>
-            <div className="share-button-container">
-                <KakaoShareButton/>
-            </div>
         </>
     )
 }
