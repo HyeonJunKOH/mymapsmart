3c56329398b7135a8cf52f69e3bfc551e1aa15df
KOHHyeonJun <tttmmm100@naver.com>
2024-06-28 14:44:58 +0900

    api키 롤백

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-const BASE_URL1 = "https://apis.data.go.kr/6300000/openapi2022/tourspot/"
-const BASE_URL2 = "https://apis.data.go.kr/6300000/openapi2022/shppg/"
-const BASE_URL3 = "https://apis.data.go.kr/6300000/openapi2022/restrnt/"
-const BASE_URL4 = "https://apis.data.go.kr/6300000/openapi2022/tourroms/"
+const BASE_URL1 = "https://apis.data.go.kr/6300000/openapi2022/tourspot/gettourspot"
+const BASE_URL2 = "https://apis.data.go.kr/6300000/openapi2022/shppg/getshppg"
+const BASE_URL3 = "https://apis.data.go.kr/6300000/openapi2022/restrnt/getrestrnt"
+const BASE_URL4 = "https://apis.data.go.kr/6300000/openapi2022/tourroms/gettourroms"
 
-export const TourUrl = `${BASE_URL1}gettourspot?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
-export const ShpUrl = `${BASE_URL2}getshppg?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
-export const RestUrl = `${BASE_URL3}getrestrnt?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
-export const RomUrl = `${BASE_URL4}gettourroms?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
+export const TourUrl = `${BASE_URL1}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
+export const ShpUrl = `${BASE_URL2}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
+export const RestUrl = `${BASE_URL3}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
+export const RomUrl = `${BASE_URL4}?serviceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&numOfRows=1000`
 
